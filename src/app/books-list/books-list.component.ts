bb45e407223696ef01c7a6a4fa433b05e8c97e9a
Jordan <48123323+JordanSOA@users.noreply.github.com>
2019-07-17 10:32:20 +0200

    2nd attempt to api book

--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -10,9 +10,26 @@ export class BooksListComponent implements OnInit {
   public books = [];
 
   ngOnInit() {
+  console.log(this.booksService.getBooks());
+  this.books.push(this.booksService.getBooks());
+  console.log(this.books);
   }
   constructor(private booksService: BooksService ) {
-    this.books = this.booksService.getBooks();
-    console.log(this.books);
-      }
-}
+    // this.books = this.booksService.getBooks();
+    // console.log(this.books);
+    // this.fillBooks();
+   // this.books = this.booksService.getBooks();
+  }
+
+  // fillBooks() {
+  //   let i = 0;
+  //   for ( i  of this.booksService.getBooks()) {
+  //     console.log('ok');
+  //     while (i < this.books.length) {
+  //       this.books = this.booksService.livres[i].volumeInfo.title;
+  //       console.log('ok');
+  //     }
+  //   }
+  }
+
+
--- src/app/books.service.ts
+++ src/app/books.service.ts
@@ -7,20 +7,32 @@ import { Title } from '@angular/platform-browser';
 })
 export class BooksService {
 
-  livres = [];
+  public livres = [];
 
   constructor(private http: HttpClient) {
-    http.get('https://www.googleapis.com/books/v1/volumes?q=Alice').subscribe((data) => {
-      console.log(data.items);
+    /* http.get('https://www.googleapis.com/books/v1/volumes?q=Alice').subscribe((data) => {
+      // console.log(data.items);
       this.livres = data.items;
       let title = this.livres[3].volumeInfo.title;
-      console.log(title);
+      //console.log(title);
       console.log(this.getBooks());
-    });
+    }); */
+    this.getBooks();
   }
   getBooks() {
     // return [ this.livres[2].volumeInfo.title ];
-    return [this.livres];
+    this.http.get('https://www.googleapis.com/books/v1/volumes?q=Alice').subscribe((data) => {
+      //console.log(Object.getOwnPropertyNames(data));
+      //console.log(Object.keys(data));
+      let ok = data.items[0].volumeInfo;
+      //console.log(ok);
+      for (let [key, value] of Object.entries(ok)) {
+        console.log(`${key}: ${value}`);
+      }
+      // let title = this.livres[0].volumeInfo.title;
+      //console.log(title);
+    });
+    return this.livres;
     }
   }
 
