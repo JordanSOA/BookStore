7255277329c60fe3dee44a806c6a0cede165c94a
Jordan <48123323+JordanSOA@users.noreply.github.com>
2019-07-22 21:32:48 +0200

    cookie handling

--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BooksService } from '../books.service';
 import { ActivatedRoute } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
+// import { CookieService } from 'ngx-cookie-service';
 
 
 @Component({
@@ -12,24 +12,23 @@ import { CookieService } from 'ngx-cookie-service';
 export class BooksListComponent implements OnInit {
   public books = [];
   public bookD: object;
-  public cookieV: object;
-  today: number = Date.now();
+  // public cookieV: object;
+  // public today = Date.now();
 
   ngOnInit() {
   }
-  constructor(private booksService: BooksService, private route: ActivatedRoute, private cookieService: CookieService) {
+  constructor(private booksService: BooksService, private route: ActivatedRoute) {
   }
 
   search() {
     this.booksService.getBooks().subscribe((data) => {
       console.log(data.items);
       this.books = data.items;
-      this.cookieService.set('id', 'cookie1');
-      this.cookieService.set('value', 'TEST');
-      this.cookieV =  this.cookieService.getAll();
-      // const allCookies = this.cookieService.get(this.cookieV);
-      console.log(this.cookieV);
-      // console.log(allCookies + ' all');
+      // this.cookieService.set('id', 'value', undefined, '/');
+      // this.cookieV =  this.cookieService.getAll();
+      // // const allCookies = this.cookieService.get(this.cookieV);
+      // console.log(this.cookieV);
+      // // console.log(allCookies + ' all');
     });
   }
   searchIsbn(isbn) {
--- src/app/books.service.ts
+++ src/app/books.service.ts
@@ -1,24 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
   public query = '';
   public isbn = '';
+  public cookieV: object;
+  public today = Date.now();
 
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private cookieService: CookieService) {
   }
   getBooks(): Observable<any> {
     console.log(this.query);
+    this.createCookie();
+    console.log('https://www.googleapis.com/books/v1/volumes?q=' + this.query + '&maxResults=40');
     return (this.http.get('https://www.googleapis.com/books/v1/volumes?q=' + this.query + '&maxResults=40'));
   }
   getBookByIsbn(isbn): Observable<any> {
     console.log('https://www.googleapis.com/books/v1/volumes?q=+' + this.isbn);
     return (this.http.get('https://www.googleapis.com/books/v1/volumes?q=+isbn+' + this.isbn + ' '));
-    // return this.http.get(url);
+  }
+  createCookie() {
+    this.cookieService.set('id', 'voila', undefined, '/');
+    this.cookieService.set('lookesfor', 'https://www.googleapis.com/books/v1/volumes?q=' + this.query + '&maxResults=40');
+    this.cookieV = this.cookieService.getAll();
+    console.log(this.cookieV);
   }
 
 }
